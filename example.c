09f1c53f5a9a359cdf39fbc5c03c2cdbb8d48469
Kyle Dean <kdean003@ucr.edu>
2020-06-26 10:26:17 -0700

    first commit

---
+++ example.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int cond = 1;
+	if(cond)
+		printf("Condition is true...!\n");
+	else
+		printf("Condition is false...!\n");
+
+	printf("End of program\n");
+	return(0);
+}
